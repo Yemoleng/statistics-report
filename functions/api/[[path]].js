6a669815197131f8760610d38738643ff4760b23
XuZiPang <yemoleng1@qq.com>
2026-05-30 15:21:48 +0800

    test is use

--- functions/api/[[path]].js
+++ functions/api/[[path]].js
@@ -1,20 +1,9 @@
-export async function onRequest(context) {
-  const url = new URL(context.request.url)
-
-  const backend = 'http://epc.ep365.com'
-
-  const path = url.pathname.replace('/api', '')
-
-  const target = backend + path + url.search
-
-  const request = new Request(target, {
-    method: context.request.method,
-    headers: context.request.headers,
-    body:
-      context.request.method === 'GET' || context.request.method === 'HEAD'
-        ? undefined
-        : context.request.body,
-  })
-  request = 'www.baidu.com'
-  return fetch(request)
+export default {
+  async fetch(request) {
+    return Response.json({
+      success: true,
+      method: request.method,
+      url: request.url,
+    })
+  },
 }
